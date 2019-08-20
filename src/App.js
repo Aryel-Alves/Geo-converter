f0970476c137df66b7408852d6c517578f2befe1
Aryel Alves <Aryel-Alves@users.noreply.github.com>
2019-08-20 00:37:49 -0300

    added text field

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+/*
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [125.6, 10.1]
+    },
+    "properties": {
+      "name": "Dinagat Islands"
+    }
+  }
+*/
+
 function App() {
+
+  const [userInput, setUserInput] = useState("")
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Geobuf / GeoJson converter </p>
+        <textarea className="text-input">
+          Enter a GeoJson here!
+        </textarea>
+        <input type="submit" value="convert !" className="btn-input"/>
       </header>
     </div>
   );
