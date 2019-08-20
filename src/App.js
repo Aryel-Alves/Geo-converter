2bdab96911f21fcc2822d2186e70fb12039ce477
Aryel Alves <mr.aryel@gmail.com>
2019-08-20 17:52:49 -0300

    added geobuf library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import axios from "axios";
+import geobuf from 'geobuf';
+import Pbf from 'pbf';
 import './App.css';
 
 /*
@@ -16,23 +19,44 @@ import './App.css';
 
 function App() {
 
-  const [userInput, setUserInput] = useState("a")
+  const [userInput, setUserInput] = useState("")
 
-  function logSHIT(algo){
+  /*function logSHIT(algo){
     console.log(algo)
+  }*/
+
+  function handleChange(event) {
+    setUserInput(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    var buffer = geobuf.decode(new Pbf(userInput));
+    console.log(buffer);
+    let headers = {
+      headers: {
+        'Accept': "application/octet-stream"
+      }
+    }
+    axios.get('http://ggt-des.ibge.gov.br/api/bcim/aldeias-indigenas/623', headers)
+    .then(res => {
+      console.log(res.data)
+      let parsedRes = geobuf.decode(new Pbf(res.data))
+      console.log(parsedRes)
+    })
+    event.preventDefault();
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <p> Geobuf / GeoJson converter </p>
-        <textarea className="text-input" onChange={setUserInput}>
-          Enter a GeoJson here!
-        </textarea>
-        <input type="submit" value="convert !" className="btn-input"/>
-        <button onClick={() => logSHIT(userInput)}>
-          Click me
-        </button>
+        <textarea className="text-input" onChange={handleChange}/>
+        <input 
+          type="submit" 
+          value="convert !" 
+          className="btn-input" 
+          onClick={handleSubmit}
+        />
       </header>
     </div>
   );
